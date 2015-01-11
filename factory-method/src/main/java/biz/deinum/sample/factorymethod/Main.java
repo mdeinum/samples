88f2ba8311b5d5b795fa58ab43cec0112e9d31eb
Marten Deinum <mdeinum@gmail.com>
2015-01-11 11:05:23 +0100

    Seperate modules for samples. Update to Spring Boot 1.2.1

--- factory-method/src/main/java/biz/deinum/sample/factorymethod/Main.java
+++ factory-method/src/main/java/biz/deinum/sample/factorymethod/Main.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
@@ -11,9 +12,8 @@ import org.springframework.context.annotation.ImportResource;
 /**
  * Created by marten on 18-02-14.
  */
-@EnableAutoConfiguration
 @ImportResource("classpath:spring-factory.xml")
-@Configuration
+@SpringBootApplication
 public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
